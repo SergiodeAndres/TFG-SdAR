85b5ae5bfc191044945382fa6dc3c345d2aa8c1a
SergiodeAndres <91533687+SergiodeAndres@users.noreply.github.com>
2024-02-21 12:01:45 +0100

    Inicio de sesión de empleados

--- tfg-frontend/src/main/java/es/proyecto/tfgfrontend/controller/EmpleadosController.java
+++ tfg-frontend/src/main/java/es/proyecto/tfgfrontend/controller/EmpleadosController.java
@@ -1,8 +1,10 @@
 package es.proyecto.tfgfrontend.controller;
 import es.proyecto.tfgfrontend.model.Empleado;
+import es.proyecto.tfgfrontend.model.Sitio;
 import es.proyecto.tfgfrontend.paginator.PageRender;
 import es.proyecto.tfgfrontend.service.IEmpleadoService;
 import es.proyecto.tfgfrontend.service.ISitioService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/frontend")
@@ -29,19 +33,34 @@ class EmpleadosController {
     }
 
     @PostMapping("/comprobarEmpleado")
-    public String comprobarEmpleado(Model model, @RequestParam String correoEmpleado, @RequestParam String passwordEmpleado) {
+    public String comprobarEmpleado(Model model, @RequestParam String correoEmpleado, @RequestParam String passwordEmpleado,
+                                    RedirectAttributes attributes, HttpSession session) {
         Empleado empleado = empleadoService.buscarPorEmail(correoEmpleado);
         if (empleado == null) {
-            model.addAttribute("error", "Correo no existente");
-            return "paginas/inicioSesionEmpleado";
+            attributes.addFlashAttribute("msg", "Correo no existente.");
+            return "redirect:/frontend/inicioSesionEmpleado";
         }
-        else {
-            empleado = empleadoService.buscarPorEmailYPassword(correoEmpleado, passwordEmpleado);
-            if (empleado == null) {
-                model.addAttribute("error", "Contraseña incorrecta");
-                return "paginas/inicioSesionEmpleado";
+        empleado = empleadoService.buscarPorEmailYPassword(correoEmpleado, passwordEmpleado);
+        if (empleado == null) {
+            attributes.addFlashAttribute("msg", "Contraseña incorrecta.");
+            return "redirect:/frontend/inicioSesionEmpleado";
+        }
+        List<Empleado> gerentes = empleadoService.buscarPorGerenteVerdadero();
+        session.setAttribute("gerente", false);
+        for (Empleado g: gerentes) {
+            if (g.getDni().equals(empleado.getDni())) {
+                session.setAttribute("gerente", true);
             }
         }
-        return "paginas/inicioSesionEmpleado";
+        Sitio sitioEmpleado = empleado.getSitioID();
+        session.setAttribute("sitio", sitioEmpleado);
+        return "redirect:/frontend/homeEmpleado";
+    }
+
+    @GetMapping("/homeEmpleado")
+    public String homeEmpleado(Model model, HttpSession session) {
+        Boolean gerente = (Boolean) session.getAttribute("gerente");
+        model.addAttribute("gerente", gerente);
+        return "paginas/homeEmpleado";
     }
 }
